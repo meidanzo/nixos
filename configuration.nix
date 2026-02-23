f2eed4cac44c138872d5a6eb6b211bb69aece984
meidanzo <meidanzo@foxmail.com>
2026-02-24 05:40:11 +0800

    add editor

--- configuration.nix
+++ configuration.nix
@@ -100,9 +100,12 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  git
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    git
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    neovim
+    emacs
+    emacs-gtk
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
